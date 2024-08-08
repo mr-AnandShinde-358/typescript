52f023aa1a7cc5f613078646084a3720c583b174
mr-AnandShinde-358 <asprogramer@gmail.com>
2024-08-08 23:56:04 +0530

    Array

---
+++ 02basic/myArray.js
@@ -0,0 +1,11 @@
+// define array
+var superHeros = [];
+// const heroPower:number[] = []
+var heroPower = [];
+var allUsers = [];
+allUsers.push({ name: "anand", isActive: false });
+superHeros.push("hellow");
+var MLModels = [
+    [255, 255, 255],
+    []
+];
---
+++ 02basic/myArray.ts
@@ -0,0 +1,21 @@
+// define array
+
+const superHeros:string[] = []
+// const heroPower:number[] = []
+const heroPower:Array<number> = []
+
+type User = {
+    name:string,
+    isActive:boolean
+}
+
+const allUsers :User[]=[]
+
+allUsers.push({name:"anand",isActive:false})
+superHeros.push("hellow")
+
+const MLModels :number[][]=[
+    [255,255,255],
+    []
+]
+
