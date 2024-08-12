f10fe2d163bc93e993b196519942535f21499c71
mr-AnandShinde-358 <asprogramer@gmail.com>
2024-08-12 18:05:27 +0530

    Getter and Setter

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -14,8 +14,25 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Jaipur";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNumber;
     }
 }
 const hitesh = new User("h@h.com", "hitesh");
 // hitesh.city="Jaipur"
+// hitesh.deleteToken()
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -12,17 +12,37 @@
 // }
 
 class User{
-   
-     readonly city:string="Jaipur"
+
+    private _courseCount =1
+    readonly city:string="Jaipur"
     constructor(
         public email:string,
         public name:string,
         // private userId:string
     ){
     }
+
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNumber){
+        if(courseNumber<=1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount=courseNumber
+    }
 }
 
 const hitesh = new User("h@h.com","hitesh")
 // hitesh.city="Jaipur"
+// hitesh.deleteToken()
 
 
