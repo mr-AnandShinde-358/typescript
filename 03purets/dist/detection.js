3b683e1b23d7be915e21ec76b59920ae12cb4759
mr-AnandShinde-358 <asprogramer@gmail.com>
2024-08-13 15:30:12 +0530

    Instanceof and Type Predicates

--- 03purets/dist/detection.js
+++ 03purets/dist/detection.js
@@ -31,3 +31,25 @@ function isAdminAccount(account) {
     }
     return account.name;
 }
+// instanceof narrowing
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim() !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird Food";
+    }
+}
--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -28,23 +28,51 @@ function printAll(strs: string | string[] | null) {
   }
 }
 
-
 // The in operator narrowing
 
-interface User{
-    name:string,
-    email:string
+interface User {
+  name: string;
+  email: string;
 }
 
-interface Admin{
-    name:string,
-    email:string,
-    isAdmin:boolean
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
 }
 
-function isAdminAccount(account:User|Admin){
-    if("isAdmin" in account){
-        return account.isAdmin
-    }
-    return account.name
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+  return account.name;
+}
+
+// instanceof narrowing
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// Predicates
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim() !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird Food";
+  }
 }
