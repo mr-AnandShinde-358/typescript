5ae15f0e568ee6cf89c6b0e063477dba3443fc2e
mr-AnandShinde-358 <asprogramer@gmail.com>
2024-08-13 12:13:07 +0530

    Generics in Array and Arrow functions

--- 03purets/dist/myGenerics.js
+++ 03purets/dist/myGenerics.js
@@ -15,4 +15,18 @@ function identityFour(val) {
     return val;
 }
 identityFour(5);
-identityFour({});
+// identityFour<Bootle>({})
+// Generics in Array and Arrow functions
+function getSearchProducts(products) {
+    // do some database operations
+    const myIndex = 3;
+    return products[myIndex];
+}
+// const getMoreSearchProducts =()=>{}
+// const getMoreSearchProducts =<T>(products:T[]):T=>{
+const getMoreSearchProducts = (products) => {
+    // do some database operation
+    const myIndex = 4;
+    products.length;
+    return products[myIndex];
+};
--- 03purets/src/myGenerics.ts
+++ 03purets/src/myGenerics.ts
@@ -1,29 +1,48 @@
-const score:Array<number> = []
-const names:Array<string> =[]
+const score: Array<number> = [];
+const names: Array<string> = [];
 
-function identityOne(val:boolean|number):boolean|number{
-    return val
+function identityOne(val: boolean | number): boolean | number {
+  return val;
 }
 
-function identityTwo(val:any):any{
-    return val
+function identityTwo(val: any): any {
+  return val;
 }
 
-function identityThree<Type>(val:Type):Type{
-    return val
+function identityThree<Type>(val: Type): Type {
+  return val;
 }
 
 // identityThree(true)
 
-function identityFour<T>(val:T):T{
-    return val
+function identityFour<T>(val: T): T {
+  return val;
 }
 
-identityFour(5)
+identityFour(5);
 
-interface Bootle{
-    brand:string,
-    type:number
+interface Bootle {
+  brand: string;
+  type: number;
 }
 
-identityFour<Bootle>({})
+// identityFour<Bootle>({})
+
+// Generics in Array and Arrow functions
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+
+// const getMoreSearchProducts =()=>{}
+
+// const getMoreSearchProducts =<T>(products:T[]):T=>{
+const getMoreSearchProducts =<T,>(products:T[]):T=>{ // <T ,> this comma is not ordinary tag just like p tag or a tag this is "Generic"
+    // do some database operation
+    const myIndex = 4
+    products.length
+    return products[myIndex]
+}
